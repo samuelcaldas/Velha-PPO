5b1d3c329bbb6f2baa68b87d309a06c8c2b592fc
Samuel Caldas <samuel.caldas@gmail.com>
2022-05-24 00:42:53 -0300

    Split Class and Env

---
+++ cartpole.py
@@ -0,0 +1,140 @@
+"""
+Title: Proximal Policy Optimization
+Author: [Ilias Chrysovergis](https://twitter.com/iliachry)
+Date created: 2021/06/24
+Last modified: 2021/06/24
+Description: Implementation of a Proximal Policy Optimization agent for the CartPole-v0 environment.
+"""
+
+"""
+## Introduction
+
+This code example solves the CartPole-v0 environment using a Proximal Policy Optimization (PPO) agent.
+
+### CartPole-v0
+
+A pole is attached by an un-actuated joint to a cart, which moves along a frictionless track.
+The system is controlled by applying a force of +1 or -1 to the cart.
+The pendulum starts upright, and the goal is to prevent it from falling over.
+A reward of +1 is provided for every timestep that the pole remains upright.
+The episode ends when the pole is more than 15 degrees from vertical, or the cart moves more than 2.4 units from the center.
+After 200 steps the episode ends. Thus, the highest return we can get is equal to 200.
+
+[CartPole-v0](https://gym.openai.com/envs/CartPole-v0/)
+
+### Proximal Policy Optimization
+
+PPO is a policy gradient method and can be used for environments with either discrete or continuous action spaces.
+It trains a stochastic policy in an on-policy way. Also, it utilizes the actor critic method. The actor maps the
+observation to an action and the critic gives an expectation of the rewards of the agent for the observation given.
+Firstly, it collects a set of trajectories for each epoch by sampling from the latest version of the stochastic policy.
+Then, the rewards-to-go and the advantage estimates are computed in order to update the policy and fit the value function.
+The policy is updated via a stochastic gradient ascent optimizer, while the value function is fitted via some gradient descent algorithm.
+This procedure is applied for many epochs until the environment is solved.
+
+![Algorithm](https://i.imgur.com/rd5tda1.png)
+
+- [PPO Original Paper](https://arxiv.org/pdf/1707.06347.pdf)
+- [OpenAI Spinning Up docs - PPO](https://spinningup.openai.com/en/latest/algorithms/ppo.html)
+
+### Note
+
+This code example uses Keras and Tensorflow v2. It is based on the PPO Original Paper,
+the OpenAI's Spinning Up docs for PPO, and the OpenAI's Spinning Up implementation of PPO using Tensorflow v1.
+
+[OpenAI Spinning Up Github - PPO](https://github.com/openai/spinningup/blob/master/spinup/algos/tf1/ppo/ppo.py)
+"""
+
+"""
+## Libraries
+
+For this example the following libraries are used:
+
+1. `Proximal_Policy_Optimization` for the PPO model
+3. `gym` for getting everything we need about the environment
+"""
+
+import gym
+import Proximal_Policy_Optimization
+
+"""
+## Hyperparameters
+"""
+
+# Hyperparameters of the PPO algorithm
+steps_per_epoch = 4000
+epochs = 30
+gamma = 0.99
+clip_ratio = 0.2
+policy_learning_rate = 3e-4
+value_function_learning_rate = 1e-3
+train_policy_iterations = 80
+train_value_iterations = 80
+target_kl = 0.01
+hidden_sizes = (64, 64)
+
+# True if you want to render the environment
+render = False
+
+"""
+## Initializations
+"""
+
+# Initialize the environment and get the dimensionality of the
+# observation space and the number of possible actions
+env = gym.make("CartPole-v0")
+observation_dimensions = env.observation_space.shape[0]
+num_actions = env.action_space.n
+
+# Initialize the observation, episode return and episode length
+observation = env.reset()
+episode_return = 0
+episode_length = 0
+
+# Initialize the PPO class
+ppo = Proximal_Policy_Optimization.PPO(observation_dimensions, hidden_sizes, num_actions, policy_learning_rate, value_function_learning_rate,
+                                       steps_per_epoch, train_policy_iterations, train_value_iterations, target_kl, clip_ratio)
+
+"""
+## Run
+"""
+# Iterate over the number of epochs
+for epoch in range(epochs):
+    # Initialize the sum of the returns, lengths and number of episodes for each epoch
+    sum_return = 0
+    sum_length = 0
+    num_episodes = 0
+
+    # Iterate over the steps of each epoch
+    for step in range(steps_per_epoch):
+        if render:
+            env.render()
+
+        # Get the logits, action, and take one step in the environment
+        _action = ppo.get_action(observation.reshape(1, -1))
+        observation_new, reward, done, _ = env.step(_action)
+        episode_return += reward
+        episode_length += 1
+
+        # Store the transition
+        ppo.store(reward)
+
+        # Update the observation
+        observation = observation_new
+
+        # If the episode is done, reset the environment
+        if done or (step == steps_per_epoch - 1):
+            ppo.finish_epoch(done, observation.reshape(1, -1))
+            sum_return += episode_return
+            sum_length += episode_length
+            num_episodes += 1
+            observation_new = env.reset()
+            episode_return = 0
+            episode_length = 0
+
+    ppo.train()
+
+    # Print mean return and length for each epoch
+    print(
+        f" Epoch: {epoch + 1}. Mean Return: {sum_return / num_episodes}. Mean Length: {sum_length / num_episodes}"
+    )
