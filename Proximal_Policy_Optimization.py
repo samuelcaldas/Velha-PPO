e0ee8704a970fb698dc41c602124183b122c0a60
Samuel Caldas <samuel.caldas@gmail.com>
2022-05-26 02:00:56 -0300

    Split buffer in enother file

--- Proximal_Policy_Optimization.py
+++ Proximal_Policy_Optimization.py
@@ -11,8 +11,7 @@ For this example the following libraries are used:
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-import numpy as np
-import scipy.signal
+import Buffer
 
 
 class make:
@@ -26,11 +25,11 @@ class make:
         self.clip_ratio = clip_ratio
 
         # Initialize the buffer
-        self.buffer = self.Buffer(self.observation_dimensions, steps_per_epoch)
+        self.buffer = Buffer.make(self.observation_dimensions, steps_per_epoch)
 
         # Initialize the actor and the critic as keras models
         observation_input = keras.Input(
-            shape=(self.observation_dimensions,), dtype=tf.float32)
+            shape=self.observation_dimensions, dtype=tf.float32)
         logits = self.build_feedforward_neural_network(
             observation_input, list(hidden_sizes) + [self.num_actions], tf.tanh, None)
         self.actor = keras.Model(inputs=observation_input, outputs=logits)
@@ -151,64 +150,3 @@ class make:
         # Update the value function
         for _ in range(self.train_value_iterations):
             self.train_value_function(observation_buffer, return_buffer)
-
-    class Buffer:
-        # Buffer for storing trajectories
-        def __init__(self, observation_dimensions, size, gamma=0.99, lam=0.95):
-            # Buffer initialization
-            self.observation_buffer = np.zeros(
-                (size, observation_dimensions), dtype=np.float32
-            )
-            self.action_buffer = np.zeros(size, dtype=np.int32)
-            self.advantage_buffer = np.zeros(size, dtype=np.float32)
-            self.reward_buffer = np.zeros(size, dtype=np.float32)
-            self.return_buffer = np.zeros(size, dtype=np.float32)
-            self.value_buffer = np.zeros(size, dtype=np.float32)
-            self.logprobability_buffer = np.zeros(size, dtype=np.float32)
-            self.gamma, self.lam = gamma, lam
-            self.pointer, self.trajectory_start_index = 0, 0
-
-        def discounted_cumulative_sums(self, x, discount):
-            # Discounted cumulative sums of vectors for computing rewards-to-go and advantage estimates
-            return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
-
-        def store(self, observation, action, reward, value, logprobability):
-            # Append one step of agent-environment interaction
-            self.observation_buffer[self.pointer] = observation
-            self.action_buffer[self.pointer] = action
-            self.reward_buffer[self.pointer] = reward
-            self.value_buffer[self.pointer] = value
-            self.logprobability_buffer[self.pointer] = logprobability
-            self.pointer += 1
-
-        def finish_trajectory(self, last_value=0):
-            # Finish the trajectory by computing advantage estimates and rewards-to-go
-            path_slice = slice(self.trajectory_start_index, self.pointer)
-            rewards = np.append(self.reward_buffer[path_slice], last_value)
-            values = np.append(self.value_buffer[path_slice], last_value)
-
-            deltas = rewards[:-1] + self.gamma * values[1:] - values[:-1]
-
-            self.advantage_buffer[path_slice] = self.discounted_cumulative_sums(
-                deltas, self.gamma * self.lam)
-            self.return_buffer[path_slice] = self.discounted_cumulative_sums(rewards, self.gamma)[
-                :-1]
-
-            self.trajectory_start_index = self.pointer
-
-        def get(self):
-            # Get all data of the buffer and normalize the advantages
-            self.pointer, self.trajectory_start_index = 0, 0
-            advantage_mean, advantage_std = (
-                np.mean(self.advantage_buffer),
-                np.std(self.advantage_buffer),
-            )
-            self.advantage_buffer = (
-                self.advantage_buffer - advantage_mean) / advantage_std
-            return (
-                self.observation_buffer,
-                self.action_buffer,
-                self.advantage_buffer,
-                self.return_buffer,
-                self.logprobability_buffer,
-            )
